6ae009a60a3cc02bfc5478c7ecc9ce9e57154d1b
Gaonkar30 <akashggaonkar@gmail.com>
2024-07-13 12:07:53 +0530

    final files

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const Schema = mongoose.Schema;
 const UserSchema=new Schema({
     userName:{type:String,min:4,required:true},
     password:{type:String,required:true},
